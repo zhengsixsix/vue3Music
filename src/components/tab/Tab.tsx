5dc3378554dd59dc14b495ec4a48e43e2616d29c
1314520zjX! <1833496831@qq.com>
2022-11-24 20:07:40 +0800

    增加侧边栏

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -6,43 +6,59 @@
  * @FilePath: \vue3Music\src\components\tab\Tab.tsx
  * @Description:
  */
-import { defineComponent, reactive, ref } from "vue";
+import { defineComponent, nextTick, onMounted, reactive, ref } from "vue";
 import style from "./Tab.module.less";
 const Tab = defineComponent({
   setup() {
-    const defaultActive = ref<string>("");
+    let defaultActive = ref<string>("");
+    let asideMode = ref<string>("");
     const asider = reactive([
       {
         title: "发现音乐",
         icon: "iconfont icon-yinle",
+        id: "1",
       },
       {
         title: "视频",
         icon: "iconfont icon-yinle",
+        id: "2",
       },
       {
         title: "收藏",
         icon: "iconfont icon-yinle",
+        id: "3",
       },
       {
         title: "每日推荐",
         icon: "iconfont icon-yinle",
+        id: "4",
       },
     ]);
+    let bodyWidth = ref<number>(0);
+    onMounted(() => {
+      nextTick(() => {
+        let bodys: HTMLBodyElement | null = document.querySelector("body");
+        if (bodys !== null) {
+          bodyWidth.value = bodys.clientWidth;
+          if (bodyWidth.value <= 900) {
+            console.log(123);
+            asideMode.value = "horizontal";
+          } else {
+            asideMode.value = "vertical";
+          }
+        }
+      });
+    });
     return () => (
       <div class={style.container}>
         <div class={style.aside}>
           <el-menu
             class={style.menu_vertical_demo}
+            mode={asideMode.value}
             default-active={defaultActive.value}
           >
             {asider.map((item) => {
-              return (
-                <el-menu-item index={item.title}>
-                  <i class={item.icon}></i>
-                  <span>{item.title}</span>
-                </el-menu-item>
-              );
+              return <el-menu-item index={item.id}>{item.title}</el-menu-item>;
             })}
           </el-menu>
         </div>
